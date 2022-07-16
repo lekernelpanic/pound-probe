9ee0c883c16d0e02a6e13d6ad4fd8aafd42e50e0
leKernelPanic <kevin.tosi@protonmail.com>
2022-07-16 18:06:31 +0200

    Basic character

---
+++ probe.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+var POWER = 80
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left"):
+		apply_impulse(Vector2 (), Vector2(-POWER, 0))
+	if Input.is_action_pressed("right"):
+		apply_impulse(Vector2 (), Vector2(POWER, 0))
+	if Input.is_action_pressed("up"):
+		apply_impulse(Vector2 (), Vector2(0, -POWER))
+	if Input.is_action_pressed("down"):
+		apply_impulse(Vector2 (), Vector2(0, POWER))
