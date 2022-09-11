2c314bc45c11399538cae1303856f849cf5663a7
leKernelPanic <kevin.tosi@protonmail.com>
2022-09-11 15:50:48 +0200

    Deletion of the impulse mechanism

--- sound-probe/probe/probe.gd
+++ sound-probe/probe/probe.gd
@@ -1,9 +1,7 @@
 extends RigidBody2D
 
-var POWER = 40
-var IMPULSE_CONSUMPTION = 10
-var REFILL_SPEED = 40
-
+var POWER = 200
+var MAX_SPEED = 500
 var UNZOOM_SCALE = 128
 var ZOOM_SMOOTHNESS = 0.005
 var MIN_ZOOM = 1.5
@@ -11,27 +9,21 @@ var MAX_ZOOM = 3
 
 var zoom = 0.5
 
-func _input(event):
-	if probe.rcs >= IMPULSE_CONSUMPTION:
-		if event.is_action_pressed("left"):
-			apply_impulse(Vector2 (), Vector2(-POWER, 0))
-			probe.rcs -= IMPULSE_CONSUMPTION
-		if event.is_action_pressed("right"):
-			apply_impulse(Vector2 (), Vector2(POWER, 0))
-			probe.rcs -= IMPULSE_CONSUMPTION
-		if event.is_action_pressed("up"):
-			apply_impulse(Vector2 (), Vector2(0, -POWER))
-			probe.rcs -= IMPULSE_CONSUMPTION
-		if event.is_action_pressed("down"):
-			apply_impulse(Vector2 (), Vector2(0, POWER))
-			probe.rcs -= IMPULSE_CONSUMPTION
-
 func _process(delta):
+	var power = delta * POWER
+	
+	if Input.is_action_pressed("left"):
+		apply_impulse(Vector2 (), Vector2(-power, 0))
+	if Input.is_action_pressed("right"):
+		apply_impulse(Vector2 (), Vector2(power, 0))
+	if Input.is_action_pressed("up"):
+		apply_impulse(Vector2 (), Vector2(0, -power))
+	if Input.is_action_pressed("down"):
+		apply_impulse(Vector2 (), Vector2(0, power))
+	
 	var new_zoom = clamp(linear_velocity.length() / UNZOOM_SCALE, MIN_ZOOM, MAX_ZOOM)
 	zoom = lerp(zoom, new_zoom, ZOOM_SMOOTHNESS)
 	$camera.zoom = Vector2(zoom, zoom)
 	
-	if probe.rcs < 100:
-		probe.rcs += delta * REFILL_SPEED
-	else:
-		probe.rcs = 100
+	if linear_velocity.length() > MAX_SPEED + 1:
+		linear_velocity = linear_velocity.normalized() * MAX_SPEED
--- sound-probe/ui/ui.gd
+++
@@ -1,4 +0,0 @@
-extends CanvasLayer
-
-func _process(_delta):
-	$h_box_container/slider.value = probe.rcs
