461f2f7817e65a5a5d041a7d77648d07bfabfeb4
leKernelPanic <kevin.tosi@protonmail.com>
2022-09-14 20:34:40 +0200

    Thruster particles

--- sound-probe/probe/probe.gd
+++ sound-probe/probe/probe.gd
@@ -12,14 +12,24 @@ var zoom = 0.5
 func _process(delta):
 	var power = delta * POWER
 	
+	$particles.rotation = -rotation
+	$particles/left.emitting = false
+	$particles/right.emitting = false
+	$particles/up.emitting = false
+	$particles/down.emitting = false
+	
 	if Input.is_action_pressed("left"):
 		apply_impulse(Vector2 (), Vector2(-power, 0))
+		$particles/right.emitting = true
 	if Input.is_action_pressed("right"):
 		apply_impulse(Vector2 (), Vector2(power, 0))
+		$particles/left.emitting = true
 	if Input.is_action_pressed("up"):
 		apply_impulse(Vector2 (), Vector2(0, -power))
+		$particles/down.emitting = true
 	if Input.is_action_pressed("down"):
 		apply_impulse(Vector2 (), Vector2(0, power))
+		$particles/up.emitting = true
 	
 	var new_zoom = clamp(linear_velocity.length() / UNZOOM_SCALE, MIN_ZOOM, MAX_ZOOM)
 	zoom = lerp(zoom, new_zoom, ZOOM_SMOOTHNESS)
