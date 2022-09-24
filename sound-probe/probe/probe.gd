147cef283157359b24f8ea0ed413a0110cbd6a87
leKernelPanic <kevin.tosi@protonmail.com>
2022-09-24 19:32:53 +0200

    Probe adjustements

--- sound-probe/probe/probe.gd
+++ sound-probe/probe/probe.gd
@@ -1,11 +1,9 @@
 extends RigidBody2D
 
-var POWER = 200
-var MAX_SPEED = 500
+var POWER = 60
 var UNZOOM_SCALE = 128
 var ZOOM_SMOOTHNESS = 0.005
-var MIN_ZOOM = 1.5
-var MAX_ZOOM = 3
+var NORMAL_ZOOM = 2
 var MIN_COLLISION_DB = 10
 
 var zoom = 0.5
@@ -23,13 +21,9 @@ func _process(delta):
 		apply_impulse(Vector2 (), Vector2(0, -power))
 	if Input.is_action_pressed("down"):
 		apply_impulse(Vector2 (), Vector2(0, power))
-	
-	var new_zoom = clamp(linear_velocity.length() / UNZOOM_SCALE, MIN_ZOOM, MAX_ZOOM)
-	zoom = lerp(zoom, new_zoom, ZOOM_SMOOTHNESS)
+
+	zoom = lerp(zoom, NORMAL_ZOOM, ZOOM_SMOOTHNESS)
 	$camera.zoom = Vector2(zoom, zoom)
-	
-	if linear_velocity.length() > MAX_SPEED + 1:
-		linear_velocity = linear_velocity.normalized() * MAX_SPEED
 
 func _on_probe_body_entered(body):
 	if !$collision.playing:
