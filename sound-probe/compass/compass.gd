6033e18794ddd4f6dc0baa90d7e14130fe21b363
leKernelPanic <kevin.tosi@protonmail.com>
2022-10-07 20:29:02 +0200

    Vanish commpass on destination

--- sound-probe/compass/compass.gd
+++ sound-probe/compass/compass.gd
@@ -4,12 +4,19 @@ export(NodePath) var origin
 export(NodePath) var destination
 export var destination_parallax_scale = 1.0
 
-var MAX_DISTANCE = 20000
+var MAX_DISTANCE = 40000
 var MIN_ALPHA = 0.04
+var VANISH_DISTANCE = 12000
+var VANISHED_DISTANCE = 6000
 
 func _process(_delta):
 	var origin_position = get_node(origin).global_position
 	var destination_position =  get_node(destination).position / destination_parallax_scale
+	var distance = origin_position.distance_to(destination_position)
 
-	modulate.a = clamp(1 - (origin_position.distance_to(destination_position)) / MAX_DISTANCE, MIN_ALPHA, 1)
+	if distance > VANISH_DISTANCE:
+		modulate.a = clamp(1 - distance / MAX_DISTANCE, MIN_ALPHA, 1)
+	else:
+		modulate.a = max((distance - VANISHED_DISTANCE) / VANISH_DISTANCE, 0)
+	
 	rotation = origin_position.angle_to_point(destination_position)
