18d48449f0d75fda5af657dcc71259c7c66530e4
leKernelPanic <kevin.tosi@protonmail.com>
2022-10-23 20:12:06 +0200

    Visualizer

--- sound-probe/jupiter/description/description.gd
+++ sound-probe/jupiter/description/description.gd
@@ -5,6 +5,7 @@ export var appear = false
 var ALPHA = 0.4
 
 func _process(delta):
+	
 	if appear:
 		visible = true
 		$panel.modulate.a = clamp($panel.modulate.a + delta, 0, ALPHA)
@@ -12,3 +13,9 @@ func _process(delta):
 		$panel.modulate.a = clamp($panel.modulate.a - delta, 0, ALPHA)
 		if $panel.modulate.a <= 0:
 			visible = false
+	
+	var viewport_rect = get_viewport().get_visible_rect().size
+	$panel/visualizer.position.y = viewport_rect.y
+	
+	$panel/visualizer.size = viewport_rect
+	$panel/visualizer.size.y /= 4
