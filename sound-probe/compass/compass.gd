c9a9c1b3e634732d8b1fd1af982a427fe782bf90
leKernelPanic <kevin.tosi@protonmail.com>
2022-10-27 20:43:15 +0200

    GDScript style for compass

--- sound-probe/compass/compass.gd
+++ sound-probe/compass/compass.gd
@@ -1,17 +1,29 @@
 extends Node2D
+# Applies the rotation from the origin to destination
+
+
+const MAX_DISTANCE = 40_000
+const MIN_ALPHA = 0.04
+const VANISH_DISTANCE = 12_000
+const VANISHED_DISTANCE = 6_000
 
 export(NodePath) var origin
 export(NodePath) var destination
 export var destination_parallax_scale = 1.0
 
-var MAX_DISTANCE = 40000
-var MIN_ALPHA = 0.04
-var VANISH_DISTANCE = 12000
-var VANISHED_DISTANCE = 6000
+var _origin_node
+var _destination_node
+
+
+func _ready():
+	_origin_node = get_node(origin)
+	_destination_node = get_node(destination)
+
 
 func _process(_delta):
-	var origin_position = get_node(origin).global_position
-	var destination_position =  get_node(destination).position / destination_parallax_scale
+	var origin_position = _origin_node.global_position
+	var destination_position =  _destination_node.position
+	destination_position /= destination_parallax_scale
 	var distance = origin_position.distance_to(destination_position)
 
 	if distance > VANISH_DISTANCE:
