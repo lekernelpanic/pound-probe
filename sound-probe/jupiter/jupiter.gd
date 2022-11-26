b7e10f3a24beb74261f982e06b7475f8a632101c
leKernelPanic <kevin.tosi@protonmail.com>
2022-11-26 11:34:33 +0100

    Juno probe transition

--- sound-probe/jupiter/jupiter.gd
+++ sound-probe/jupiter/jupiter.gd
@@ -16,6 +16,7 @@ func _ready():
 func _on_jupiter_body_entered(body):
 	if(body.is_in_group("probe")):
 		$description.appear = true
+		body.probe = "juno"
 
 
 func _on_jupiter_body_exited(body):
--- sound-probe/probe/probe.gd
+++ sound-probe/probe/probe.gd
@@ -8,12 +8,24 @@ const UNZOOM_SCALE = 128
 const ZOOM_SMOOTHNESS = 0.005
 const NORMAL_ZOOM = 2
 const MIN_COLLISION_DB = 10
+const APPARITION_SPEED = 2
+
+export(String, "voyager_1", "juno") var probe = "voyager_1" setget set_probe
 
 var _zoom
+var _probes
 
 
 func _init():
 	_zoom = INITIAL_ZOOM
+
+
+func _ready():
+	_probes = {	
+		"voyager_1": $voyager_1,
+		"juno": $juno,
+	}
+	set_probe(probe)
 
 
 func _process(delta):
@@ -29,9 +41,29 @@ func _process(delta):
 		apply_impulse(Vector2 (), Vector2(0, -power))
 	if Input.is_action_pressed("down"):
 		apply_impulse(Vector2 (), Vector2(0, power))
-
+	
 	_zoom = lerp(_zoom, NORMAL_ZOOM, ZOOM_SMOOTHNESS)
 	$camera.zoom = Vector2(_zoom, _zoom)
+	
+	for probe_name in _probes:
+		var probe_material = _probes[probe_name].get_child(0).material
+		var apparition_level = probe_material.get_shader_param("apparition")
+		if(probe_name == probe):
+			apparition_level = apparition_level + delta / APPARITION_SPEED
+		else:
+			apparition_level = apparition_level - delta / APPARITION_SPEED
+		
+		apparition_level = clamp(apparition_level, 0, 1)
+		probe_material.set_shader_param("apparition", apparition_level)
+
+
+func set_probe(new_probe):
+	if(is_inside_tree()):
+		var probe_node = _probes[probe]
+		var new_probe_node = _probes[new_probe]
+		probe_node.call_deferred("set_disabled", true)
+		new_probe_node.call_deferred("set_disabled", false)
+	probe = new_probe
 
 
 func _on_probe_body_entered(body):
