b59ab02f57c54791b2d4775df72a7ca73daf6d2e
leKernelPanic <kevin.tosi@protonmail.com>
2023-03-29 19:23:35 +0200

    Typed methods in main

--- sound-probe/main.gd
+++ sound-probe/main.gd
@@ -2,12 +2,12 @@ extends Node2D
 # General to the game.
 
 
-func _ready():
+func _ready() -> void:
 	_scale_adaptation()
 	get_tree().get_root().size_changed.connect(_scale_adaptation)
 
 
-func _input(event):
+func _input(event) -> void:
 	if event.is_action_pressed("toggle_fullscreen"):
 		if get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN:
 			get_window().mode = Window.MODE_WINDOWED
@@ -24,10 +24,10 @@ func _input(event):
 		$ui/exit.visible = false
 
 
-func _on_exit_timer_timeout():
+func _on_exit_timer_timeout() -> void:
 	get_tree().quit()
 
 
-func _scale_adaptation():
+func _scale_adaptation() -> void:
 	var size: float = get_window().size.y
 	get_window().content_scale_factor = clamp(800.0 / size, 1, 1.5)
