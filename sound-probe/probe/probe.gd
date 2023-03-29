545525973aa2dff1c7898483a6eb2b112d99ae9c
leKernelPanic <kevin.tosi@protonmail.com>
2023-03-29 20:10:49 +0200

    Refactor probe GDScript

--- sound-probe/probe/probe.gd
+++ sound-probe/probe/probe.gd
@@ -4,54 +4,43 @@ extends RigidBody2D
 
 signal collision(force)
 
-enum probes {voyager_1, juno}
-
 const APPARITION_SPEED: float = 2
 const MINIMAL_COLLISION_FORCE: float = 200
 
-@export var probe : probes : set = set_probe
+@export_enum("voyager_1", "juno") var probe = "voyager_1" : set = set_probe
 
 var _probe_nodes: Dictionary
+var _transiting: bool
 
 
 func _ready() -> void:
 	_probe_nodes = {
-		probes["voyager_1"]: $voyager_1,
-		probes["juno"]: $juno,
+		"voyager_1": $voyager_1,
+		"juno": $juno,
 	}
 	set_probe(probe)
 
 
-func _process(delta) -> void:
-	
-	if(Input.is_action_just_pressed("ui_accept")):
-		print(position)
-	
-	for probe_name in _probe_nodes:
-		var probe_node: Node2D = _probe_nodes[probe_name].get_child(0)
-		var probe_mat: ShaderMaterial = probe_node.material
-		var apparition: float = probe_mat.get_shader_parameter("apparition")
-		if(probe_name == probe):
-			apparition = apparition + delta / APPARITION_SPEED
-		else:
-			apparition = apparition - delta / APPARITION_SPEED
-		
-		apparition = clamp(apparition, 0, 1)
-		probe_mat.set_shader_parameter("apparition", apparition)
+func _process(delta: float) -> void:
+	if _transiting:
+		_transiting = false
+		for probe_name in _probe_nodes:
+			_transiting = _manage_tranition(probe_name, delta) or _transiting
 
 
-func set_probe(new_probe) -> void:
+func set_probe(new_probe: String) -> void:
 	if is_inside_tree():
 		_probe_nodes[probe].call_deferred("set_disabled", true)
 		_probe_nodes[new_probe].call_deferred("set_disabled", false)
 	probe = new_probe
+	_transiting = true
 
 
-func _on_probe_body_entered(body) -> void:
+func _on_probe_body_entered(body: Node2D) -> void:
 	emit_signal("collision", (linear_velocity - body.linear_velocity).length())
 
 
-func _on_probe_collision(force) -> void:
+func _on_probe_collision(force: float) -> void:
 	if force >= MINIMAL_COLLISION_FORCE:
 		$recover_timer.wait_time = force / 100
 		$recover_timer.start()
@@ -69,9 +58,25 @@ func _on_recover_timer_timeout() -> void:
 	_activate_probe(true)
 
 
-func _activate_probe(activated) -> void:
+func _activate_probe(activated: bool) -> void:
 	$thrusters.activated = activated
 	$light.enabled = activated
 	for probe_name in _probe_nodes:
 		_probe_nodes[probe_name].get_child(0).material.set_shader_parameter(
 				"on", activated)
+
+
+func _manage_tranition(probe_name: String, delta: float) -> bool:
+	var probe_node = _probe_nodes[probe_name].get_child(0)
+	var probe_mat: ShaderMaterial = probe_node.material
+	var apparition: float = probe_mat.get_shader_parameter("apparition")
+	
+	if probe_name == probe:
+		apparition = apparition + delta / APPARITION_SPEED
+	else:
+		apparition = apparition - delta / APPARITION_SPEED
+	
+	apparition = clamp(apparition, 0, 1)
+	probe_mat.set_shader_parameter("apparition", apparition)
+	
+	return apparition > 0 and apparition < 1
